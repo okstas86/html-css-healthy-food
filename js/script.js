f855f854c86bd116bfcc02ca962b4cae86b3065c
okstas86 <stanislavocunev1@gmail.com>
2024-04-13 16:32:30 +0300

    Added script.js

---
+++ js/script.js
@@ -0,0 +1,75 @@
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+
+yearEl.textContent = currentYear
+
+const btnNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header")
+
+btnNavEl.addEventListener("click", () => {
+	headerEl.classList.toggle("nav-open")
+})
+
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link")
+allLinks.forEach(link =>
+	link.addEventListener("click", e => {
+		e.preventDefault()
+		const href = link.getAttribute("href")
+		if (href === "#")
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			})
+		if (href !== "#" && href.startsWith("#")) {
+			const sectionEl = document.querySelector(href)
+			sectionEl.scrollIntoView({
+				behavior: "smooth",
+			})
+		}
+		if (link.classList.contains("main-nav-link"))
+			headerEl.classList.toggle("nav-open")
+	})
+)
+
+//stycky navigation
+const sectionHeroEl = document.querySelector(".section-hero")
+
+const obs = new IntersectionObserver(
+	entries => {
+		const ent = entries[0]
+		if (!ent.isIntersecting) {
+			document.body.classList.add("stycky")
+		}
+		if (ent.isIntersecting) {
+			document.body.classList.remove("stycky")
+		}
+	},
+	{
+		root: null,
+		threshold: 0,
+		rootMargin: "-180px",
+	}
+)
+obs.observe(sectionHeroEl)
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+
+function checkFlexGap() {
+	var flex = document.createElement("div")
+	flex.style.display = "flex"
+	flex.style.flexDirection = "column"
+	flex.style.rowGap = "1px"
+
+	flex.appendChild(document.createElement("div"))
+	flex.appendChild(document.createElement("div"))
+
+	document.body.appendChild(flex)
+	var isSupported = flex.scrollHeight === 1
+	flex.parentNode.removeChild(flex)
+	console.log(isSupported)
+
+	if (!isSupported) document.body.classList.add("no-flexbox-gap")
+}
+checkFlexGap()
